30eaa8e56677b7c43438abde51db8df8f152333e
Alex O R <alex.oregan@teamwork.com>
2018-11-19 15:36:00 +0000

    main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
+import pandas
 
-def main():
+
+NUM_TRAINING_EXAMPLES = 5000
+NUM_TEST_EXAMPLES = 1000
+DATA_PATH = "../assets/kickstarter-projects/ks-projects-201801.csv"
+
+
+
+def __main__():
+    print("sdfsddf")
     return
 
-
+__main__()
 
