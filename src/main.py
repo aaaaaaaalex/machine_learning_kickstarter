87d37bedc416475c9c8f0dd552e82d3886bf6b71
Alex O Regan <alexsoregan@gmail.com>
2018-11-23 17:04:35 +0000

    ensemble classification abandoned, calculate f-scores of both classifiers independently

--- src/main.py
+++ src/main.py
@@ -5,11 +5,19 @@ from sklearn.feature_extraction import text as sktext
 
 import random
 from datetime import datetime
+import tkinter
+import matplotlib.pyplot as plt
 
 NUM_TRAINING_EXAMPLES = 5000
 NUM_TEST_EXAMPLES = 1000
 DATA_PATH = "../assets/kickstarter-projects/ks-projects-201801.csv"
 RANDOM_SEED = 42
+NUM_KFOLDS = 5
+
+# save fitted-vectorizer
+COUNT_VECTOR = {}
+NB_ACCURACY = -1
+KNN_ACCURACY = -1
 
 
 # reads data from a file, specify how many random rows to read with num_samples
@@ -19,8 +27,8 @@ def read_in_data(filename, total_rows, num_samples):
 
     data_file = open(filename)
     data = pd.read_csv(data_file,
-            #skiprows=skip_rows,
-            skiprows=total_rows-num_samples,
+            skiprows=skip_rows,
+            #skiprows=total_rows-num_samples,
             usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12],
             names=[
                 "id",
@@ -96,12 +104,14 @@ def split_dataframe(df, rand_seed=RANDOM_SEED):
 
 # takes a dataframe of data relevant to KNN classification and returns a trained classifier
 def train_classifier(df, classifier_type):
+    global COUNT_VECTOR
+    global KNN_ACCURACY
+    global NB_ACCURACY
     X_train, X_test, y_train, y_test = split_dataframe(df)
 
 
     # train
     if classifier_type is 'KNN':
-        print(X_train)
         classifier = neighbors.KNeighborsClassifier(n_neighbors=5)
 
     elif classifier_type is "NB":
@@ -111,8 +121,9 @@ def train_classifier(df, classifier_type):
         tfid = sktext.TfidfTransformer()
 
         temp = cvctr.fit_transform( np.reshape(X_train, -1) )
+        COUNT_VECTOR = cvctr #save the fitted vectorizer
+        
         X_train = tfid.fit_transform(temp)
-
         X_test = cvctr.transform( np.reshape(X_test, -1) )
 
     classifier.fit(X_train, y_train)
@@ -120,28 +131,82 @@ def train_classifier(df, classifier_type):
     # test
     accuracy = classifier.score( X_test, y_test )
 
+    if classifier_type is "NB":
+        NB_ACCURACY = accuracy
+    elif classifier_type is "KNN":
+        KNN_ACCURACY = accuracy
+
     return classifier, accuracy
 
 
+def get_num_true_positives(cls_predictions, cls_labels):
+    # filter answers that were correctly True
+    temp_answers_correct = (cls_predictions == cls_labels)
+    temp_correct_answers = cls_predictions[ temp_answers_correct ]
+    true_pos = len( temp_correct_answers[ temp_correct_answers ])
 
-def ensemble_classify(knn_classifier, nb_classifier, knn_df, nb_df):
+    return true_pos
+
+def get_num_false_positives(cls_predictions, cls_labels):
+    # filter answers that were incorrectly True
+    temp_answers_incorrect = (cls_predictions != cls_labels)
+    temp_incorrect_answers = cls_predictions[ temp_answers_incorrect ]
+    false_pos = len(temp_incorrect_answers[ temp_incorrect_answers ])
+
+    return false_pos
+
+
+# evaluate and compare both KNN and NB algorithms
+def evaluate_algorithms(knn_classifier, nb_classifier, knn_df, nb_df):
+    global COUNT_VECTOR
+
     knn_X_train, knn_X_test, knn_y_train, knn_y_test = split_dataframe(knn_df)
     nb_X_train,  nb_X_test,  nb_y_train,  nb_y_test  = split_dataframe(nb_df)
 
-    knn_results = knn_classifier.predict(knn_X_test)
-
     # prepare naive bayes input data
-    cvctr = sktext.CountVectorizer()
-    temp = cvctr.fit_transform( np.reshape(nb_X_train, -1) ) # this step must be done to give CountVectorizer the same vocabulary as in previous tests
+    cvctr = COUNT_VECTOR
     nb_X_test = cvctr.transform( np.reshape(nb_X_test, -1) )
 
-    nb_results = nb_classifier.predict(nb_X_test)
+    #---------------------------------------------------------average the probabilities of both classifiers
+    nb_answers = nb_classifier.predict(nb_X_test)
+    knn_answers = knn_classifier.predict(knn_X_test)
 
-    print("knn_results", knn_results)
-    print("nb_results", nb_results)
+    # get precision
+    nb_precision  = np.mean( nb_answers == nb_y_test )
+    knn_precision = np.mean(knn_answers == knn_y_test)
 
-    return 1
+    #get True Positives and Recall values
+    nb_true_pos  = get_num_true_positives ( nb_answers, nb_y_test)
+    nb_false_pos = get_num_false_positives( nb_answers, nb_y_test)
+    nb_recall = nb_true_pos / ( nb_true_pos + nb_false_pos )    
 
+    knn_true_pos  = get_num_true_positives ( knn_answers, knn_y_test)
+    knn_false_pos = get_num_false_positives( knn_answers, knn_y_test)
+    knn_recall = knn_true_pos / ( knn_true_pos + knn_false_pos )
+
+    # determine F-score
+    knn_f = 2 * ( (knn_recall * knn_precision) / (knn_recall + knn_precision) )
+    nb_f  = 2 * ( (nb_recall * nb_precision) / (nb_recall + nb_precision) )
+
+    return knn_recall, nb_recall, knn_f, nb_f
+
+
+def plot_barchart(knn_scores, nb_scores):
+    x =  [ u'KNN Precision', u'KNN Recall Value', u'KNN F-Score', u'']
+    y =  [ knn_scores['accuracy'], knn_scores['recall'], knn_scores['fscore'], 0]
+
+    x1 = [ u'Naive Bayes Precision', u'Naive Bayes Recall Value', u'Naive Bayes F-Score']
+    y1 = [ nb_scores['accuracy'],  nb_scores['recall'],  nb_scores['fscore']]
+
+    plt.bar(x, y, label="KNN precision, recall, and f-score")
+    plt.bar(x1, y1, label="Naive Bayes precision, recall and f-score")
+
+    plt.title("Comparison of Accuracy, Recall Value, and F-Score of K-NN and Naive Bayes Algorithms")
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.show()
+
+    return
 
 
 def __main__():
@@ -162,11 +227,26 @@ def __main__():
     nb_classifier, bayes_accuracy = train_classifier(nb_filtered, "NB")
 
     # test ensemble classification
-    ensemble_accuracy = ensemble_classify(knn_classifier, nb_classifier, knn_filtered, nb_filtered)
+    knn_recall, nb_recall, knn_f, nb_f = evaluate_algorithms(knn_classifier, nb_classifier, knn_filtered, nb_filtered)
 
     print("KNN accuracy", knn_accuracy)
+    print("KNN F-Score", knn_f)
+    print("KNN Recall Value", knn_recall, end="\n\n")
     print("Bayes accuracy", bayes_accuracy)
-    print("Ensemble Accuracy", ensemble_accuracy)
+    print("Bayes F-Score", nb_f)
+    print("Bayes Recall Value", nb_recall, end="\n\n")
+
+    plot_barchart({
+            'accuracy': knn_accuracy,
+            'recall': knn_recall,
+            'fscore': knn_f
+        },
+        {
+            'accuracy': bayes_accuracy,
+            'recall': nb_recall,
+            'fscore': nb_f
+        })
+
     return
 
 __main__()
